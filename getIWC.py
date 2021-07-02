8a09a852e464e494cae4c4b03ea415f837f7e274
lyk <liang1996@tju.edu.cn>
2021-07-02 17:46:39 +0800

    fix issue

--- getIWC.py
+++ getIWC.py
@@ -1,54 +1,73 @@
 # -*- coding: utf-8 -*-
-import re
-from time import sleep
-import pandas as pd
-from github import Github
-import random
 
+import re
+import random
+from time import sleep
+
+from github import Github
+import pandas as pd
+
+# Enter your Github token
 user_token = [
-    '60298e09a51074cbf6d482f50ad975dbe0121a0f',
-    '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',
-    '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',
-    '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'
+    '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',
+    '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',
+    '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'
 ]
 
 g = Github(user_token[random.randint(0, len(user_token) - 1)])
 
-issueIds = []
-relations = []
 
-# 通过关键字来查找库,爬取多个相关库
+# calling the search repo API
 def get_repositories(keywords):
     repositories = g.search_repositories(query=keywords)
     print('get repositories {}'.format(repositories.totalCount))
+
     return repositories
 
-# 遍历issues
-def get_issue_info(repo_issues, repo_name, repo_owner):
-    issue_info_list = set()
+
+# 遍历issues，获取issue与commit信息
+def get_relations(repo_issues, repo_name, repo_owner):
+    relations = []
     for issue in repo_issues:
-
-        # search commits from issue description
-
-        # if(commits.size == 1 && commit.message contains issueId):
-        #     relation = <issue_url, commit_url>
-
-
-        print(issue)
-        issue_patch = issue.pull_request.patch_url
+        issue_number = issue.number
+        issue_title = issue.title
+        issue_title.encode('utf-8', 'ignore')
+        issue_label = ''
+        if issue.labels != None:
+            for label in issue.labels:
+                issue_label = label.name
+                issue_label.encode('utf-8', 'ignore')
         issue_html_url = issue.html_url
         issue_html_url.encode('utf-8', 'ignore')
-        comments = issue.get_comments()
-        for comment in comments:
-            if '#' in comment.body:
-                number = re.findall(r"(?<=\#)\d+", comment.body)
-                if number not in issueIds and issue_patch is not None:
-                    issueIds.append(number)
-                    issue_info_list.add(issue_patch)
-            break
-        print(issueIds)
-        print(issue_patch)
-    return issue_info_list
+
+        issue_events = issue.get_events()
+        commit_ids = []
+        for issue_event in issue_events:
+            if issue_event.commit_id is not None:
+                commit_id = issue_event.commit_id
+                commit_url = issue_event.commit_url
+                commit_ids.append(commit_id)
+        print(commit_ids)
+        if len(commit_ids) == 1:
+            commit_info = repo.get_commit(commit_id)
+            commit_message = commit_info.commit.message
+            if '#' in commit_message and str(issue_number) in commit_message:
+                #commit_number = re.findall(r"(?<=\#)\d+", commit_message)
+                commit_url = commit_info.html_url
+                relation_tup = (issue_html_url, commit_url)
+            relations.append(relation_tup)
+        else:
+            for cid in commit_ids:
+                commit_info = repo.get_commit(commit_id)
+                commit_message = commit_info.commit.message
+                if '#' in commit_message and str(issue_number) in commit_message:
+                    # commit_number = re.findall(r"(?<=\#)\d+", commit_message)
+                    commit_url = commit_info.html_url
+                    relation_tup = (issue_html_url, commit_url)
+                break
+            relations.append(relation_tup)
+    print(relations)
+    return relations
 
 
 if __name__ == '__main__':
@@ -56,39 +75,35 @@ if __name__ == '__main__':
     repositories = get_repositories(keywords)
     flag = 0
     for repo in repositories:
-        issueIds.clear()
         repo_name = repo.name
-        repo_star = repo.stargazers_count
-        repo_fork = repo.forks_count
         repo_name.encode('utf-8', 'ignore')
-        print(repo_name, repo_star, repo_fork)
+        print(repo_name)
         repo_owner = repo.owner.login
         repo_owner.encode('utf-8', 'ignore')
 
-        # 获取repo的全部closed状态的issue
         repo_issues = repo.get_issues(state='closed')
-        issue_info_list = []
+        relations_info = []
+
         try:
-            issue_info_list = get_issue_info(repo_issues, repo_name, repo_owner)
+            relations_info = get_relations(repo_issues, repo_name, repo_owner)
         except Exception as e:
+            sleep(180)
             g = Github(user_token[random.randint(0, len(user_token) - 1)])
 
-
-        if len(issue_info_list) == 0:
+        if len(relations_info) == 0:
             continue
 
-        # flag = 1
-        fileName = 'issue_info_of_' + keywords + '.csv'
-        data = pd.DataFrame(issue_info_list)
+        fileName = 'relations_of_' + keywords + '.csv'
+        data = pd.DataFrame(relations_info)
         try:
             if flag == 0:
-                csv_headers = ['commit_or_patch_url']
+                csv_headers = ['issue_url', 'commit_url']
                 data.to_csv(fileName, header=csv_headers, index=False,
                             mode='a+', encoding='utf-8-sig')
 
             else:
                 data.to_csv(fileName, header=False, index=False,
                             mode='a+', encoding='utf-8-sig')
-            flag += 1
+                flag += 1
         except UnicodeEncodeError:
             print('Encode error drop the data')
