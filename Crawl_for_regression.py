28a6e068653965a0afac7f05aae2b10e2905a05a
lyk <liang1996@tju.edu.cn>
2021-07-02 20:50:26 +0800

    添加爬取regression Bug

--- Crawl_for_regression.py
+++ Crawl_for_regression.py
@@ -28,45 +28,56 @@ def get_repositories(keywords):
 
 # 遍历issues，获取issue与commit信息
 def get_relations(repo_issues, repo_name, repo_owner):
+    label_for_issue = None
     relations = []
     for issue in repo_issues:
         issue_number = issue.number
         issue_title = issue.title
         issue_title.encode('utf-8', 'ignore')
-        issue_label = ''
-        if issue.labels is not None:
-            for label in issue.labels:
-                issue_label = label.name
-                issue_label.encode('utf-8', 'ignore')
         issue_html_url = issue.html_url
         issue_html_url.encode('utf-8', 'ignore')
         # 去除掉Pull Request，只要issue
         if 'pull' not in issue_html_url:
-            issue_events = issue.get_events()
-            commit_ids = []
-            for issue_event in issue_events:
-                if issue_event.commit_id is not None:
-                    commit_id = issue_event.commit_id
-                    commit_ids.append(commit_id)
-            print(commit_ids)
-            if len(commit_ids) == 1:
-                commit_info = repo.get_commit(commit_id)
-                commit_message = commit_info.commit.message
-                if '#' in commit_message and str(issue_number) in commit_message:
-                    # commit_number = re.findall(r"(?<=\#)\d+", commit_message)
-                    commit_url = commit_info.html_url
-                    relation_tup = (issue_html_url, commit_url)
-                relations.append(relation_tup)
+            issue_label = ''
+            if issue.labels is not None:
+                for label in issue.labels:
+                    issue_label = label.name
+                    issue_label.encode('utf-8', 'ignore')
+                    if issue_label == 'regression':
+                        label_for_issue = 1
+                    break
             else:
-                for cid in commit_ids:
+                comments = issue.get_comments()
+                for comment in comments:
+                    if 'regression' in comment.body:
+                        label_for_issue = 0
+                    break
+            if label_for_issue is not None:
+                issue_events = issue.get_events()
+                commit_ids = []
+                for issue_event in issue_events:
+                    if issue_event.commit_id is not None:
+                        commit_id = issue_event.commit_id
+                        commit_ids.append(commit_id)
+                print(commit_ids)
+                if len(commit_ids) == 1:
                     commit_info = repo.get_commit(commit_id)
                     commit_message = commit_info.commit.message
                     if '#' in commit_message and str(issue_number) in commit_message:
                         # commit_number = re.findall(r"(?<=\#)\d+", commit_message)
                         commit_url = commit_info.html_url
-                        relation_tup = (issue_html_url, commit_url)
-                    break
-                relations.append(relation_tup)
+                        relation_tup = (repo_name, issue_html_url, commit_url, label_for_issue)
+                    relations.append(relation_tup)
+                else:
+                    for cid in commit_ids:
+                        commit_info = repo.get_commit(commit_id)
+                        commit_message = commit_info.commit.message
+                        if '#' in commit_message and str(issue_number) in commit_message:
+                            # commit_number = re.findall(r"(?<=\#)\d+", commit_message)
+                            commit_url = commit_info.html_url
+                            relation_tup = (repo_name, issue_html_url, commit_url, label_for_issue)
+                        break
+                    relations.append(relation_tup)
     print(relations)
     return relations
 
@@ -98,7 +109,7 @@ if __name__ == '__main__':
         data = pd.DataFrame(relations_info)
         try:
             if flag == 0:
-                csv_headers = ['issue_url', 'commit_url']
+                csv_headers = ['repo_name', 'issue_url', 'commit_url', 'label']
                 data.to_csv(fileName, header=csv_headers, index=False,
                             mode='a+', encoding='utf-8-sig')
 
